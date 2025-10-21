aa963d4fa23221bf7a970caf3c1ffd9046d66346
221 <221@PC-Klass.ru>
2025-10-21 14:10:04 +0300

    lb11

--- PasswordLibrary/Password.cs
+++ PasswordLibrary/Password.cs
@@ -5,6 +5,8 @@ namespace PasswordLibrary
     public static class Password
     {
         public static bool IsPasswordValidate(string password)
-            => password.Length >= 8 && Regex.IsMatch(password, @"\d+[a-z]+");
+            => password.Length >= 8 && Regex.IsMatch(password, @"^*\d+[a-z]+[A-Z]+[\W]");
+
+        public static bool IsVibe() => throw new NotImplementedException();
     }
 }
