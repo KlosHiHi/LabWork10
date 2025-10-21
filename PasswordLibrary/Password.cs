cfe7752de38a374e45ba98732679b1640efcab2d
221 <221@PC-Klass.ru>
2025-10-21 13:57:46 +0300

    lb10

---
+++ PasswordLibrary/Password.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordLibrary
+{
+    public static class Password
+    {
+        public static bool IsPasswordValidate(string password)
+            => password.Length >= 8 && Regex.IsMatch(password, @"\d+[a-z]+");
+    }
+}
